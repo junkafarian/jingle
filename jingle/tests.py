54d93e55c032f468bd0bf003e21d87934a0aa2ad
junkafarian <fergus.doyle@largeblue.com>
2009-07-28 18:59:36 +0100

    tests

--- jingle/tests.py
+++ jingle/tests.py
@@ -2,33 +2,33 @@ import unittest
 
 from repoze.bfg import testing
 
-class ViewIntegrationTests(unittest.TestCase):
-    """ These tests are integration tests for the view.  These test
-    the functionality the view *and* its integration with the rest of
-    the repoze.bfg framework.  They cause the entire environment to be
-    set up and torn down as if your application was running 'for
-    real'.  This is a heavy-hammer way of making sure that your tests
-    have enough context to run properly, and it tests your view's
-    integration with the rest of BFG.  You should not use this style
-    of test to perform 'true' unit testing as tests will run faster
-    and will be easier to write if you use the testing facilities
-    provided by bfg and only the registrations you need, as in the
-    above ViewTests.
-    """
-    def setUp(self):
-        """ This sets up the application registry with the
-        registrations your application declares in its configure.zcml
-        (including dependent registrations for repoze.bfg itself).
-        """
-        testing.cleanUp()
-        import jingle
-        import zope.configuration.xmlconfig
-        zope.configuration.xmlconfig.file('configure.zcml',
-                                          package=jingle)
+# class ViewIntegrationTests(unittest.TestCase):
+#     """ These tests are integration tests for the view.  These test
+#     the functionality the view *and* its integration with the rest of
+#     the repoze.bfg framework.  They cause the entire environment to be
+#     set up and torn down as if your application was running 'for
+#     real'.  This is a heavy-hammer way of making sure that your tests
+#     have enough context to run properly, and it tests your view's
+#     integration with the rest of BFG.  You should not use this style
+#     of test to perform 'true' unit testing as tests will run faster
+#     and will be easier to write if you use the testing facilities
+#     provided by bfg and only the registrations you need, as in the
+#     above ViewTests.
+#     """
+#     def setUp(self):
+#         """ This sets up the application registry with the
+#         registrations your application declares in its configure.zcml
+#         (including dependent registrations for repoze.bfg itself).
+#         """
+#         testing.cleanUp()
+#         import jingle
+#         import zope.configuration.xmlconfig
+#         zope.configuration.xmlconfig.file('configure.zcml',
+#                                           package=jingle)
 
-    def tearDown(self):
-        """ Clear out the application registry """
-        testing.cleanUp()
+#     def tearDown(self):
+#         """ Clear out the application registry """
+#         testing.cleanUp()
 
 #     def test_my_view(self):
 #         from jingle.views import view_page
@@ -45,24 +45,19 @@ class ViewIntegrationTests(unittest.TestCase):
 #                                                 str(len(body))))
 
 class InternalFunctionalTests(unittest.TestCase):
-    def setUp(self):
-        """ This sets up the application registry with the
-        registrations your application declares in its configure.zcml
-        (including dependent registrations for repoze.bfg itself).
-        """
-        testing.cleanUp()
-        import jingle
-        import zope.configuration.xmlconfig
-        zope.configuration.xmlconfig.file('configure.zcml',
-                                          package=jingle)
-    
-    def tearDown(self):
-        """ Clear out the application registry """
-        testing.cleanUp()
-    
     def test_schema_registration(self):
+        from copy import deepcopy
+        from formencode.validators import UnicodeString
         from jingle.schemas import Schema
         from jingle.schemas import registry
+        class TestSchema(Schema):
+            title = UnicodeString(default=u'', not_empty=True)
+        
+        # this gets populated when the application is initialised so no we can use it directly for testing
+        registry.register('test', TestSchema())
+        self.failUnless(isinstance(registry.lookup('test'), TestSchema))
+        self.failUnless('title' in registry.lookup('test').fields)
+        
 
 
 
