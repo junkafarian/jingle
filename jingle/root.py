7fd74293fce34ed697b61b5d3138952deea588a1
junkafarian <fergus.doyle@largeblue.com>
2009-07-28 18:58:30 +0100

    initial root app doctest

--- jingle/root.py
+++ jingle/root.py
@@ -3,8 +3,18 @@ from repoze.zodbconn.finder import PersistentApplicationFinder
 
 def app(global_config, **kw):
     """ This function returns a repoze.bfg.router.Router object.
-    
-    It is usually called by the PasteDeploy framework during ``paster serve``.
+        
+        It is usually called by the PasteDeploy framework during ``paster serve``.
+        
+        >>> from repoze.bfg.router import Router
+        >>> app({})
+        Traceback (most recent call last):
+        ...
+        ValueError: No 'zodb_uri' in application configuration.
+        
+        #>>> isinstance(app({}, zodb_uri='/tmp/tmp.fs'), Router) # currently fails based on multiple configuration init
+        #True
+        
     """
     # paster app config callback
     import jingle
